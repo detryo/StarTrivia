2b2f01fef837e8ea52bf1a2506fa1d0871b35168
Chris <chris.sedano.arenas@gmail.com>
2020-05-21 11:44:06 +0100

    Import SwiftyJSOn
    using SwiftyJSON in PersonAPI.swift, modify getRandomPersonAlamo and parsePersonSwifty

--- StarTrivia/Resources/PersonAPI.swift
+++ StarTrivia/Resources/PersonAPI.swift
@@ -8,10 +8,11 @@
 
 import Foundation
 import Alamofire
+import SwiftyJSON
 
 class PersonAPI {
     
-    // Web request with Alamofire
+    // Web request with Alamofire and SwiftyJSON
     func getRandomPersonUrlAlamo(id: Int, completion: @escaping PersonResponseComplition) {
         
         guard let url = URL(string: "\(PERSON_URL)\(id)") else { return }
@@ -24,54 +25,33 @@ class PersonAPI {
                 return
             }
             
-            guard let json = response.value as? [String : Any] else { return completion(nil) }
+            guard let data = response.data else { return completion(nil) }
             
-            let person = self.parsePersonManual(json: json)
-            completion(person)
-        }
-    }
-    
-    // Wen request with URL Session
-    func getRandomPersonUrlSession(id: Int, completion: @escaping PersonResponseComplition)  {
-        
-        guard let url = URL(string: "\(PERSON_URL)\(id)") else { return }
-        
-        let task = URLSession.shared.dataTask(with: url) { (data, response, error) in
-            
-        guard error == nil else {
-            debugPrint(error.debugDescription)
-            completion(nil)
-            return
-        }
-        
-        guard let data = data else { return }
-        
-        do {
-            let jsonAny = try JSONSerialization.jsonObject(with: data, options: [])
-            guard let json = jsonAny as? [String: Any] else { return }
-            let person = self.parsePersonManual(json: json)
-            DispatchQueue.main.async {
+            do {
+                
+                let json = try JSON(data: data)
+                let person = self.parsePersonSwifty(json: json)
                 completion(person)
-            }
-            
+                
             } catch {
                 debugPrint(error.localizedDescription)
+                completion(nil)
             }
         }
-        task.resume()
     }
     
-    private func parsePersonManual(json: [String: Any]) -> Person {
-        let name = json["name"] as? String ?? ""
-        let height = json["height"] as? String ?? ""
-        let mass = json["mass"] as? String ?? ""
-        let hair = json["hair_color"] as? String ?? ""
-        let birthYear = json["birth_year"] as? String ?? ""
-        let gender = json["gender"] as? String ?? ""
-        let homeworldUrl = json["homeworld"] as? String ?? ""
-        let filmUrls = json["films"] as? [String] ?? [String]()
-        let vehicleUrls = json["vehicles"] as? [String] ?? [String]()
-        let starshipUrls = json["starships"] as? [String] ?? [String]()
+    // Parsing with SwiftyJSON
+    private func parsePersonSwifty(json: JSON) -> Person {
+        let name = json["name"].stringValue
+        let height = json["height"].stringValue
+        let mass = json["mass"].stringValue
+        let hair = json["hair_color"].stringValue
+        let birthYear = json["birth_year"].stringValue
+        let gender = json["gender"].stringValue
+        let homeworldUrl = json["homeworld"].stringValue
+        let filmUrls = json["films"].arrayValue.map({ $0.stringValue })
+        let vehicleUrls = json["vehicles"].arrayValue.map({ $0.stringValue })
+        let starshipUrls = json["starships"].arrayValue.map({ $0.stringValue })
         
         return Person(name: name, height: height, mass: mass, hair: hair, birthYear: birthYear, gender: gender, homeworldURL: homeworldUrl, filmURL: filmUrls, vehiclesURL: vehicleUrls, starshipsURL: starshipUrls)
     }
