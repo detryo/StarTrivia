9cda5b2b297c5074723d1431008b8a732809bf7b
Chris <chris.sedano.arenas@gmail.com>
2020-06-01 11:14:51 +0100

    Vehicle UI
    implement 16 labels and two buttons, pass just 8 labels and buttons to VehiclesVC

--- StarTrivia/Controller/VehiclesVC.swift
+++ StarTrivia/Controller/VehiclesVC.swift
@@ -10,11 +10,35 @@ import UIKit
 
 class VehiclesVC: UIViewController, PersonProtocol {
     
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var modelLabel: UILabel!
+    @IBOutlet weak var makerLabel: UILabel!
+    @IBOutlet weak var costLabel: UILabel!
+    @IBOutlet weak var lengthLabel: UILabel!
+    @IBOutlet weak var speedLabel: UILabel!
+    @IBOutlet weak var crewLabel: UILabel!
+    @IBOutlet weak var passangersLabel: UILabel!
+    @IBOutlet weak var previousButton: UIButton!
+    @IBOutlet weak var nextButton: UIButton!
+    
+    
+    
     var person : Person!
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
     }
-
+    
+    @IBAction func previousClicked(_ sender: UIButton) {
+        
+        
+    }
+    
+    
+    @IBAction func nextClicked(_ sender: UIButton) {
+        
+        
+    }
+    
 }
